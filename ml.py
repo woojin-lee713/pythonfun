7de80c2f98bb3640223fcf54003eec47d79e3c70
Woo Jin Lee <leewoojin@Woos-MacBook-Pro.local>
2024-03-29 14:53:33 -0400

    created a new protocol notebook and played with polymorphism

--- ml.py
+++ ml.py
@@ -52,8 +52,7 @@ def model():
     def inner_predictions(xvalues, t=0.5):
         intercept = parameters['intercept']
         slope = parameters['slope']
-        predict_proba = predict_proba_maker(slope, intercept)
-        probabilities = predict_proba(xvalues)
+        probabilities = inner_probabilities(xvalues)
         predictions = [1*(p >= t) for p in probabilities]
         return predictions
 
@@ -69,7 +68,37 @@ def model():
     )
     return methods
 
+class AbstractModel:
+    def __init__(self):
+        self.probabilities = None
+        selfpredictions = None
 
+    def fit(self, xvalues, yvalues):
+        raise NotImplementedYetError
+
+    def predict_proba(self, xvalues):
+        raise NotImplementedYetError
+
+    def predict(self, xvalues, t=0.5):
+        probabilities = self.predict_proba(xvalues)
+        self.predictions = [1*(p >= t) for p in probabilities]
+        return self.predictions
+
+class LogisticModel(AbstractModel):
+    def __init__(self):
+        super().__init__()
+        self.slope = None
+        self.intercept = None
+
+    def fit(self, xvalues, yvalues):
+        self.slope, self.intercept = (9.812037170985635, 5.175316482607568)
+
+    def predict_proba(self, xvalues):
+        self.probabilities = [1/(1 + exp(-(self.intercept + self.slope * x))) for x in xvalues]
+        return self.probabilities
+
+
+#different class
 class MLModel:
     def __init__(self):
         self.slope = None
